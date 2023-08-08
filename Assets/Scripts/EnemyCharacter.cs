f098076d7ae813025b0c22a1ce21960417f5b35d
Applayst <lyoshik@list.ru>
2023-08-08 12:45:50 +0300

    Add lerp for sit and rotate

--- Assets/Scripts/EnemyCharacter.cs
+++ Assets/Scripts/EnemyCharacter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class EnemyCharacter : Character
@@ -10,7 +11,14 @@ public class EnemyCharacter : Character
 
     public Vector3 TargetPosition { get; private set; } = Vector3.zero;
 
+    private Quaternion _rotationY;
+    private Quaternion _rotationX;
+
+    private float _deltaRY = 0;
+    private float _deltaRX = 0;
+
     private float _velocitiMagnitude = 0;
+    private bool _isActiveCoroutine;
 
     private void Start()
     {
@@ -29,13 +37,32 @@ public class EnemyCharacter : Character
             transform.position = TargetPosition;
         }
         
+        if (Math.Abs(_deltaRY) > 0.1f)
+        {
+            float maxRY = 25 * Time.deltaTime;
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, _rotationY, maxRY);           
+        }
+        else
+        {
+            transform.rotation = _rotationY;            
+        }
+
+        if (Math.Abs(_deltaRX) > 0.1f)
+        {
+            float maxRX = 25 * Time.deltaTime;
+            _head.localRotation = Quaternion.Lerp(_head.localRotation, _rotationX, maxRX);            
+        }
+        else
+        {
+            _head.rotation = _rotationX;            
+        }        
     }
 
     public void SetSitMultiplier(float sitMultiplier) => SitMultiplier = sitMultiplier;
     public void SetSpeed(float value) => Speed = value;
     public void SetMovement(in Vector3 position, in Vector3 velocity, in float averageIntervalTime)
     {
-        TargetPosition = position + (velocity * averageIntervalTime);
+        TargetPosition = position;// + (velocity * averageIntervalTime);
         _velocitiMagnitude = velocity.magnitude;
 
         this.Velocity = velocity;
@@ -43,26 +70,50 @@ public class EnemyCharacter : Character
 
     public void SetRotateX(float rotateX)
     {
-        _head.localEulerAngles = new Vector3(rotateX, 0, 0);
+        _deltaRX = _head.localEulerAngles.x - rotateX;
+        _rotationX = Quaternion.Euler(rotateX, 0, 0);
     }
 
     public void SetRotateY(float rotateY)
     {
-        transform.localEulerAngles = new Vector3(0, rotateY, 0);
+        _deltaRY = transform.localEulerAngles.y - rotateY;
+        _rotationY = Quaternion.Euler(0, rotateY, 0);        
     }
     public void SitDown()
-    {
-        Vector3 localScale = new Vector3(1f, SitMultiplier, 1f);
-        _partPoints.localScale = localScale;
-        _body.localScale = localScale;
-        _collider.localScale = new Vector3(1f, SitMultiplier * 0.6f + 0.4f, 1f);        
+    {        
+        if (_isActiveCoroutine)
+            StopCoroutine(SitAndStand(1f));
+        StartCoroutine(SitAndStand(SitMultiplier));
     }
 
     public void StandUp()
     {
-        _partPoints.localScale = Vector3.one;
-        _body.localScale = Vector3.one;
-        _collider.localScale = Vector3.one;        
+        if (_isActiveCoroutine)
+            StopCoroutine(SitAndStand(SitMultiplier));
+        StartCoroutine(SitAndStand(1f));
+    }
+
+    private IEnumerator SitAndStand(float scaleMultiplier)
+    {
+        _isActiveCoroutine = true;
+
+        Vector3 localScale = new Vector3(1f, scaleMultiplier, 1f);
+        Vector3 colliderLcalScale = new Vector3(1f, scaleMultiplier * 0.6f + 0.4f, 1f);
+
+        for (float t = 0; t < 0.1f; t += Time.deltaTime)
+        {
+            _partPoints.localScale = Vector3.Lerp(_partPoints.localScale, localScale, t);
+            _body.localScale = Vector3.Lerp(_body.localScale, localScale, t);
+            _collider.localScale = Vector3.Lerp(_collider.localScale, colliderLcalScale, t);
+
+            yield return null;
+        }
+
+        _partPoints.localScale = localScale;
+        _body.localScale = localScale;
+        _collider.localScale = colliderLcalScale;
+
+        _isActiveCoroutine = false;
     }
 
 }
